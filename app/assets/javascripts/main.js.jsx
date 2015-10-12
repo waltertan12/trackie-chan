41731dc43d8068aa5f1d92d9302a6eef4ecc145c
Walter Tan <waltertan12@gmail.com>
2015-10-12 08:36:40 -0700

    Add static pages controller

--- app/assets/javascripts/main.js.jsx
+++ app/assets/javascripts/main.js.jsx
@@ -1,12 +1,6 @@
 var landingPage = (
-  <div>
-    <div className="jumbotron">
-      <h1>Excellent Website</h1>
-    </div>
-    <h3>Reviews</h3>
-    <ul>
-      <li>"It is an excellent website" - Anonymous</li>
-    </ul>
+  <div className="jumbotron">
+    <h1>Trackie Chan</h1>
   </div>
 );
 
