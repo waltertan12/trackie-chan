4d66aaffe44e00fb552f339b71d5e6db2f4b5b74
Walter Tan <waltertan12@gmail.com>
2015-10-13 11:47:07 -0700

    Add user seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+harry = User.create(
+  username: "boywholived",
+  email: "harry.potter@hogwarts.edu",
+  password_digest: BCrypt::Password.create("password")
+)
+
+hermione = User.create(
+  username: "flimflamthezimzam",
+  email: "hermione.granger@hogwarts.edu",
+  password_digest: BCrypt::Password.create("password")
+)
+
+draco = User.create(
+  username: "simmerthezimmer",
+  email: "draco.malfoy@hogwarts.edu",
+  password_digest: BCrypt::Password.create("password")
+)
+
+ron = User.create(
+  username: "cornerthedorner",
+  email: "ronald.weasley@hogwarts.edu",
+  password_digest: BCrypt::Password.create("password")
+)
