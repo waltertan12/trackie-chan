1cc75cc9d7b57f2c20ecba4eb1acb1aec10911fe
Walter Tan <waltertan12@gmail.com>
2015-10-13 09:25:32 -0700

    Migrate users

--- db/migrate/20151013160825_create_users.rb
+++ db/migrate/20151013160825_create_users.rb
@@ -1,11 +1,17 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :email
-      t.string :password_digest
-      t.string :session_token
-      t.string :image_url
+      t.string :username, null: false, 
+                          index: true
+      t.string :email,    null: false, 
+                          index: true, 
+                          unique: true
+      t.string :password_digest, null: false 
+      t.string :session_token,   null: false, 
+                                 index: true, 
+                                 unique: true
+      t.string :image_url, null: false, 
+                           default: "http://i.imgur.com/AZuwasa.jpg?1/"
 
       t.timestamps null: false
     end
