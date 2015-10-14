730d87aba490921c6ff9fbb7485545379d9faa38
Walter Tan <waltertan12@gmail.com>
2015-10-13 20:46:35 -0700

    Style landing page

--- app/assets/javascripts/components/dashboard/dashboard.js.jsx
+++ app/assets/javascripts/components/dashboard/dashboard.js.jsx
@@ -25,7 +25,52 @@
           </div>
         );
       } else {
-        return <div></div>;
+        return (
+          <div>
+            <div className="jumbotron landing-page">
+              wow so many songs. good muzik. eargasm
+              aka trackiechan
+              <input type="search" placeholder="Explore :(" className="form-control"/>
+            </div>
+            <div className="feature">
+              <div className="row">
+                <div className="col-md-3 user-feature">
+                  Good Artist
+                </div>
+                <div className="col-md-3 user-feature">
+                  Good Artist
+                </div>
+                <div className="col-md-3 user-feature">
+                  Good Artist
+                </div>
+                <div className="col-md-3 user-feature">
+                  Good Artist
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-md-3 user-feature">
+                  Good Artist
+                </div>
+                <div className="col-md-3 user-feature">
+                  Good Artist
+                </div>
+                <div className="col-md-3 user-feature">
+                  Good Artist
+                </div>
+                <div className="col-md-3 user-feature">
+                  Good Artist
+                </div>
+              </div>
+              <div class="description">
+                <h3>You gonna have so much FOMO if you do not sign up</h3>
+                <p>no lie..</p>
+                <a href="/users/new" className="btn btn-success">
+                  Please... sign up
+                </a>
+              </div>
+            </div>
+          </div>
+        );
       }
     }
   });
