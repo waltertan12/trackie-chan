fc172815a87a3e22291a9e9b961b32bec61a37dd
Walter Tan <waltertan12@gmail.com>
2015-10-14 10:06:00 -0700

    Add seed data for tracks

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,26 +6,33 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-harry = User.create(
+harry = User.create!(
   username: "boywholived",
   email: "harry.potter@hogwarts.edu",
   password_digest: BCrypt::Password.create("password")
 )
 
-hermione = User.create(
+hermione = User.create!(
   username: "flimflamthezimzam",
   email: "hermione.granger@hogwarts.edu",
   password_digest: BCrypt::Password.create("password")
 )
 
-draco = User.create(
+draco = User.create!(
   username: "simmerthezimmer",
   email: "draco.malfoy@hogwarts.edu",
   password_digest: BCrypt::Password.create("password")
 )
 
-ron = User.create(
+ron = User.create!(
   username: "cornerthedorner",
   email: "ronald.weasley@hogwarts.edu",
   password_digest: BCrypt::Password.create("password")
+)
+
+play_by_play = Track.create!(
+  title: "Autre ne Veut - Play by Play",
+  description: "WOWOWOWOW",
+  user_id: harry.id,
+  track_url: "/media/walter/Storage/Music/Autre Ne Veut/Autre Ne Veut - Anxiety (V0)/01 - Play by Play.mp3",
 )
