5f807edd3572c6115faf1f714dcc0a6b04fb03ba
Walter Tan <waltertan12@gmail.com>
2015-10-20 13:21:47 -0700

    Create playlists assoc for tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,5 @@ class Tag < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   has_many :taggings
   has_many :tracks, through: :taggings, source: :taggable, source_type: Track 
+  has_many :playlists, through: :taggings, source: :taggable, source_type: Playlist 
 end
