c5d5d14a2e319b9c11207e0ec5a5c66f26f99396
Walter Tan <waltertan12@gmail.com>
2016-01-04 19:40:16 -0500

    Clean up code

--- app/assets/javascripts/util/api_utils.js
+++ app/assets/javascripts/util/api_utils.js
@@ -1,14 +1,16 @@
 (function (root) {
-  if (typeof root.ApiUtils === "undefined") {
+  'use strict'; 
+
+  if (typeof root.ApiUtils === 'undefined') {
     root.ApiUtils = {};
   }
 
   root.ApiUtils = {
     fetchUser: function (userId, callback) {
       $.ajax({
-        url: "/api/users/" + userId,
-        type: "GET",
-        dataType: "json",
+        url: '/api/users/' + userId,
+        type: 'GET',
+        dataType: 'json',
         success: function (user) {
           callback(user);
         },
@@ -19,17 +21,17 @@
     },
     updateUser: function (userId, params, callback, redirect) {
       $.ajax({
-        url: "/api/users/" + userId,
-        type: "PUT",
-        dataType: "json",
+        url: '/api/users/' + userId,
+        type: 'PUT',
+        dataType: 'json',
         data: {user: params},
         success: function (user) {
           callback(user);
           redirect();
         },
         error: function (err) {
-          if (err.responseText === "Not logged in error") {
-            window.location.assign("/sign_in"); 
+          if (err.responseText === 'Not logged in error') {
+            window.location.assign('/sign_in'); 
           } else {
             ErrorActions.receiveErrors(err.responseJSON);
           }
@@ -38,15 +40,15 @@
     },
     deleteUser: function (user) {
       $.ajax({
-        url: "/api/users/" + user.id,
-        type: "DELETE",
-        dataType: "json",
+        url: '/api/users/' + user.id,
+        type: 'DELETE',
+        dataType: 'json',
         success: function (user) {
 
         },
         error: function (err) {
-          if (err.responseText === "Not logged in error") {
-            window.location.assign("/sign_in"); 
+          if (err.responseText === 'Not logged in error') {
+            window.location.assign('/sign_in'); 
           } else {
             ErrorActions.receiveErrors(err.responseJSON);
           }
@@ -55,11 +57,11 @@
     },
     destroySession: function() {
       $.ajax({
-        url: "sessions",
-        dataType: "json",
-        type: "DELETE",
+        url: 'sessions',
+        dataType: 'json',
+        type: 'DELETE',
         success: function(response) {
-          window.location.assign("/");
+          window.location.assign('/');
         },
         error: function (err) {
           ErrorActions.receiveErrors(err.responseJSON);
@@ -68,15 +70,15 @@
     },
     followUser: function (user, callback) {
       $.ajax({
-        type: "POST",
-        url: "/api/followings",
+        type: 'POST',
+        url: '/api/followings',
         data: {followed_id: user.id},
         success: function (currentUser) {
           callback(currentUser);
         },
         error: function (err) {
-          if (err.responseText === "Not logged in error") {
-            window.location.assign("/sign_in"); 
+          if (err.responseText === 'Not logged in error') {
+            window.location.assign('/sign_in'); 
           } else {
             ErrorActions.receiveErrors(err.responseJSON);
           }
@@ -85,33 +87,34 @@
     },
     unfollowUser: function (user, callback) {
       $.ajax({
-        type: "DELETE",
-        url: "/api/followings",
+        type: 'DELETE',
+        url: '/api/followings',
         data: {followed_id: user.id},
         success: function (currentUser) {
           callback(currentUser);
         },
         error: function (err) {
-          if (err.responseText === "Not logged in error") {
-            window.location.assign("/sign_in"); 
+          if (err.responseText === 'Not logged in error') {
+            window.location.assign('/sign_in'); 
           } else {
             ErrorActions.receiveErrors(err.responseJSON);
           }
         }
       })
     },
+
     createComment: function (comment, trackId, callback) {
       $.ajax({
-        url: "/api/comments",
-        type: "POST",
+        url: '/api/comments',
+        type: 'POST',
         data: {comment: comment},
-        dataType: "json",
+        dataType: 'json',
         success: function (comment) {
           callback(comment, trackId);
         },
         error: function (err) {
-          if (err.responseText === "Not logged in error") {
-            window.location.assign("/sign_in"); 
+          if (err.responseText === 'Not logged in error') {
+            window.location.assign('/sign_in'); 
           } else {
             ErrorActions.receiveErrors(err.responseJSON);
           }
@@ -120,16 +123,16 @@
     },
     fetchComments: function (trackId, callback) {
       $.ajax({
-        url: "/api/comments",
-        type: "GET",
-        dataType: "json",
+        url: '/api/comments',
+        type: 'GET',
+        dataType: 'json',
         data: {track_id: trackId},
         success: function (comments) {
           callback(comments, trackId);
         },
         error: function (err) {
-          if (err.responseText === "Not logged in error") {
-            window.location.assign("/sign_in"); 
+          if (err.responseText === 'Not logged in error') {
+            window.location.assign('/sign_in'); 
           } else {
             ErrorActions.receiveErrors(err.responseJSON);
           }
